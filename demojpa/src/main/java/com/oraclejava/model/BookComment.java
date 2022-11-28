9116bafdccbfe8aff66d0902d09d2924945c09ef
HyungKu Im <damansa1@naver.com>
2022-11-29 00:41:26 +0900

    에러수정

--- demojpa/src/main/java/com/oraclejava/model/BookComment.java
+++ demojpa/src/main/java/com/oraclejava/model/BookComment.java
@@ -27,10 +27,11 @@ public class BookComment implements Serializable {
 	private Long id;   // 코멘트 id
 	
 	@NotNull(message = "이름은 필수입력입니다.")
+	@Size(min = 1, max=127, message="이름은 1자 이상 127자 이내로 입력바랍니다.")
 	private String name;	// 이름
 	
 	@NotNull(message = "코멘트는 필수입력입니다.")
-	@Size(min = 1, message = "코멘트는 필수입력입니다.")
+	@Size(min = 1, max=127, message="코멘트는 1자 이상 127자 이내로 입력바랍니다.")
 	private String content; // 코멘트 내용
 	//private Long bookId; // 북id
 	@ManyToOne
